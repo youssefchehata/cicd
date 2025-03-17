fc477a533d32e5e8a5224579019762a31ffb0af7
CHEHATA Youssef <youssef.chehata@softeam.fr>
2025-03-17 22:51:05 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          <img src={process.env.PUBLIC_URL + '/1718239085699.jpg'} alt="logo" />
+          <img src={process.env.PUBLIC_URL + '/1629585422792.jpg'} alt="logo" />
           {/* Edit <code>src/App.tsx</code> and save to reload. */}
         </p>
         <a
           className="App-link"
-          href="https://www.linkedin.com/in/nesrine-setti-5744a4127/"
+          href="https://www.linkedin.com/in/youssef-chehata-436299b5/"
           target="_blank"
           rel="noopener noreferrer"
         >
-                    Linkedin Touti
+                    Linkedin Youssef chehata
 
         </a>
       </header>
